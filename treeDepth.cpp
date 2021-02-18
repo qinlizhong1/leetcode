2b3c84e85580d9560b4bde676e7c42cec7b8b10c
lizhong <13590316596@163.com>
2021-02-18 20:14:24 +0800

    treeDepth.cpp

---
+++ treeDepth.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 覃立中 on 2021/2/18.
+//
+
+/*
+*输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，
+*最长路径的长度为树的深度。
+*/
+
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        int res = 0;
+
+        if (NULL == pRoot){
+            return 0;
+        }
+
+        return max(TreeDepth(pRoot->left), TreeDepth(pRoot->right)) + 1;
+    }
+};
